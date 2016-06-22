6e6d4f54fbc68e7651a570409894b3bf9ed5e5de
Henry Estela <henryestela@gmail.com>
2016-06-22 14:45:21 -0700

    Replace errno mod with io::Error and use Result<>
    io::Error provides last_os_error() which is equivalent to calling errno
    and strerro. One annoyance of using io::Error is that to use the
    description function, you have to import error::Error which means you
    have to import it under another name to get the error::Error trait.
    
    Using io::Error reduces the need for some libc functions and allows
    error numbers to be stored. io::Errors can be composed of each other,
    allowing for a chain of different errors to be passed up the chain.
    
    Will now prefer to use Result over Option when errno() will be set in
    error conditions.

--- src/fileops.rs
+++ src/fileops.rs
@@ -6,16 +6,18 @@ use std::ffi::CString;
 use libc::c_int;
 use std::os::unix::io::{RawFd, AsRawFd};
 use std::ops::Drop;
+use std::io::Error;
+use std::error::Error as std_error;
 
 pub struct Fd(RawFd);
 
 impl Fd {
-  pub fn open<T: Into<String>>(path: T, mode: c_int) -> Option<Fd> {
+  pub fn open<T: Into<String>>(path: T, mode: c_int) -> Result<Fd, Error> {
     let fd = unsafe { libc::open(CString::new(path.into())
                 .unwrap_or(CString::new("").unwrap()).as_ptr(), mode) };
     match fd {
-      -1 => None,
-      _ => Some(Fd(fd))
+      -1 => Err(Error::last_os_error()),
+      _ => Ok(Fd(fd))
     }
   }
 
@@ -23,10 +25,11 @@ impl Fd {
     unsafe { libc::close(self.0) }
   }
 
-  pub fn try_set_flag(&self, flag: c_int ) -> Option<c_int> {
+  // With F_SETFD we only care if fcntl failed
+  pub fn try_set_flag(&self, flag: c_int ) -> Result<c_int, Error> {
     match unsafe { libc::fcntl(self.0, libc::F_SETFD, flag) } {
-      -1 => None,
-      _ => Some(0)
+      -1 => Err(Error::last_os_error()),
+      ret @ _ => Ok(ret)
     }
   }
 }
@@ -49,6 +52,8 @@ impl Drop for Fd {
 #[test]
 // Check open/close on a file that should exist in most linux based OS.
 fn open_close_devnull() -> () {
-  let fd = Fd::open("/dev/null", libc::O_RDONLY);
-  assert!(fd.is_some());
+  match Fd::open("/dev/null", libc::O_RDONLY) {
+    Err(e) => panic!(e.description().to_owned()),
+    _ => ()
+  }
 }
--- src/ipath/service.rs
+++ src/ipath/service.rs
@@ -2,10 +2,10 @@ extern crate libc;
 
 use std::ffi::CString;
 use fileops::Fd;
-use errno::*;
-use std::os::unix::io::AsRawFd;
+use std::io::Error;
+use std::error::Error as std_error;
 
-fn ipath_context_open(unit: isize) -> Option<Fd> {
+fn ipath_context_open(unit: isize) -> Result<Fd, Error> {
   let dev_path = if unit >= 0 {
     format!("/dev/ipath{}", unit)
   } else {
@@ -15,16 +15,9 @@ fn ipath_context_open(unit: isize) -> Option<Fd> {
   // XXX: Do we need ipath_wait_for_device? it literally just waits.
 
   // Try to get a Fd and try to set the CLOEXEC flag on it.
-  let fd_maybe = Fd::open(dev_path, libc::O_RDWR);
-  match fd_maybe {
-    Some(ref fd) => {
-      if fd.try_set_flag(libc::FD_CLOEXEC).is_none() {
-        println!("{}", dump_errno_str!());
-      }
-    },
-    _ => ()
-  }
-  fd_maybe
+  let fd = try!(Fd::open(dev_path, libc::O_RDWR));
+  try!(fd.try_set_flag(libc::FD_CLOEXEC));
+  Ok(fd)
 }
 
 #[test]
@@ -33,8 +26,8 @@ fn ipath_context_open(unit: isize) -> Option<Fd> {
 #[ignore]
 // TODO: add a test for checking all available units
 fn open_close_unit_zero() {
-  let fd_maybe = ipath_context_open(0);
-  if fd_maybe.is_none() {
-    panic!(dump_errno_str!());
+  match ipath_context_open(0) {
+    Err(e) => panic!(e.description().to_owned()),
+    _ => ()
   }
 }
