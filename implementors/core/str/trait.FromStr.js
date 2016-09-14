e8e4996a435f625fc7b4c5930b16fd7f4a0acf34
Henry Estela <henryestela@gmail.com>
2016-09-14 15:42:12 -0800

    Update documentation

---
+++ implementors/core/str/trait.FromStr.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["uuid"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html' title='core::str::FromStr'>FromStr</a> for <a class='struct' href='uuid/struct.Uuid.html' title='uuid::Uuid'>Uuid</a>",];implementors["libc"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
